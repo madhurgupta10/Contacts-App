7dd950bdc347b9ccdfbab81594389b896f2be2c2
Madhur Gupta <madhurgupta10@gmail.com>
2019-07-31 16:10:48 +0530

    Completed Basic UI and Implementation for Main Activity

--- app/src/main/java/com/example/contactsapp/MainActivity.java
+++ app/src/main/java/com/example/contactsapp/MainActivity.java
@@ -1,55 +1,90 @@
 package com.example.contactsapp;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+import android.widget.Toast;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import com.example.contactsapp.helper.ContactHelper;
+import com.example.contactsapp.model.CustomContact;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+    SimpleCursorAdapter adapter;
+    Cursor cursor;
+
+    int PERMISSION_READ_WRITE_CONTACTS = 1000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initViews();
+    }
+
+    private void initViews() {
+
+        listView = findViewById(R.id.list_view);
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-    }
+        if ((checkSelfPermission(Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED)
+            && (checkSelfPermission(Manifest.permission.WRITE_CONTACTS) != PackageManager.PERMISSION_GRANTED)) {
+            requestPermissions(new String[] {
+                    Manifest.permission.WRITE_CONTACTS,
+                    Manifest.permission.READ_CONTACTS
+            }, PERMISSION_READ_WRITE_CONTACTS);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        } else {
+            showContacts();
         }
+    }
 
-        return super.onOptionsItemSelected(item);
+    private void showContacts() {
+        cursor = ContactHelper.getContactCursor(getContentResolver(), "");
+        String[] fields = new String[]{ContactsContract.Data.DISPLAY_NAME};
+        adapter = new SimpleCursorAdapter(this, android.R.layout.simple_expandable_list_item_1,
+                cursor, fields, new int[]{android.R.id.text1});
+        listView.setAdapter(adapter);
+
+        AdapterView.OnItemClickListener itemClickListener = new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long l) {
+                cursor.moveToPosition(position);
+                CustomContact customContact = new CustomContact(cursor);
+                Toast.makeText(MainActivity.this, ""+customContact.getPhoneNumber(),
+                        Toast.LENGTH_SHORT).show();
+            }
+        };
+
+        listView.setOnItemClickListener(itemClickListener);
+        adapter.notifyDataSetChanged();
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        if (requestCode == PERMISSION_READ_WRITE_CONTACTS) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                showContacts();
+            } else {
+                Toast.makeText(MainActivity.this, R.string.grant_contact_permission,
+                        Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }

75a5dd84b259cdcc69c18fc8d64ce76105ad928d
Madhur Gupta <madhurgupta10@gmail.com>
2019-07-31 14:25:55 +0530

    Added Helper Class for Contacts

---
+++ app/src/main/java/com/example/contactsapp/helper/ContactHelper.java
@@ -0,0 +1,144 @@
+package com.example.contactsapp.helper;
+
+import android.content.ContentProviderOperation;
+import android.content.ContentResolver;
+import android.content.OperationApplicationException;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.RemoteException;
+import android.provider.ContactsContract;
+
+import java.util.ArrayList;
+
+public class ContactHelper {
+
+    public static Cursor getContactCursor(ContentResolver contactHelper, String startsWith) {
+
+        String[] projection = {ContactsContract.CommonDataKinds.Phone._ID,
+                ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME,
+                ContactsContract.CommonDataKinds.Phone.NUMBER};
+
+        Cursor cursor = null;
+
+        try {
+
+            if (startsWith != null && !startsWith.equals("")) {
+                cursor = contactHelper.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                        projection,
+                        ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME + " line \"" +
+                                startsWith + "%\"",
+                        null,
+                        ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME + " ASC");
+            } else {
+                cursor = contactHelper.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                        projection,
+                        null,
+                        null,
+                        ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME + " ASC");
+            }
+
+            if (cursor != null) {
+                cursor.moveToFirst();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return cursor;
+    }
+
+    public static void createContact(ContentResolver contactHelper, String name, String number,
+                                     String email) {
+
+        ArrayList < ContentProviderOperation > operations = new ArrayList<>();
+
+        operations.add(ContentProviderOperation.newInsert(
+                ContactsContract.RawContacts.CONTENT_URI)
+                .withValue(ContactsContract.RawContacts.ACCOUNT_TYPE, null)
+                .withValue(ContactsContract.RawContacts.ACCOUNT_NAME, null)
+                .build());
+        
+        if (name != null) {
+            operations.add(ContentProviderOperation.newInsert(
+                    ContactsContract.Data.CONTENT_URI)
+                    .withValueBackReference(ContactsContract.Data.RAW_CONTACT_ID, 0)
+                    .withValue(ContactsContract.Data.MIMETYPE,
+                            ContactsContract.CommonDataKinds.StructuredName.CONTENT_ITEM_TYPE)
+                    .withValue(
+                            ContactsContract.CommonDataKinds.StructuredName.DISPLAY_NAME,
+                            name).build());
+        }
+
+        
+        if (number != null) {
+            operations.add(ContentProviderOperation.
+                    newInsert(ContactsContract.Data.CONTENT_URI)
+                    .withValueBackReference(ContactsContract.Data.RAW_CONTACT_ID, 0)
+                    .withValue(ContactsContract.Data.MIMETYPE,
+                            ContactsContract.CommonDataKinds.Phone.CONTENT_ITEM_TYPE)
+                    .withValue(ContactsContract.CommonDataKinds.Phone.NUMBER, number)
+                    .withValue(ContactsContract.CommonDataKinds.Phone.TYPE,
+                            ContactsContract.CommonDataKinds.Phone.TYPE_MOBILE)
+                    .build());
+        }
+        
+        if (email != null) {
+            operations.add(ContentProviderOperation.newInsert(ContactsContract.Data.CONTENT_URI)
+                    .withValueBackReference(ContactsContract.Data.RAW_CONTACT_ID, 0)
+                    .withValue(ContactsContract.Data.MIMETYPE,
+                            ContactsContract.CommonDataKinds.Email.CONTENT_ITEM_TYPE)
+                    .withValue(ContactsContract.CommonDataKinds.Email.DATA, email)
+                    .withValue(
+                            ContactsContract.CommonDataKinds.Email.TYPE,
+                            ContactsContract.CommonDataKinds.Email.TYPE_WORK)
+                    .build());
+        }
+        
+        try {
+            contactHelper.applyBatch(ContactsContract.AUTHORITY, operations);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void deleteContact(ContentResolver contactHelper, String number) {
+
+        ArrayList<ContentProviderOperation> operations = new ArrayList<>();
+        String[] args = new String[] {String.valueOf(getContactId(contactHelper, number))};
+        operations.add(ContentProviderOperation
+                .newDelete(ContactsContract.RawContacts.CONTENT_URI)
+                .withSelection(ContactsContract.RawContacts.CONTACT_ID + "=?", args).build());
+
+        try {
+            contactHelper.applyBatch(ContactsContract.AUTHORITY, operations);
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        } catch (OperationApplicationException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private static long getContactId(ContentResolver contactHelper, String number) {
+        Uri contactUri = Uri.withAppendedPath(ContactsContract.PhoneLookup.CONTENT_FILTER_URI,
+                Uri.encode(number));
+        String[] projection = {ContactsContract.PhoneLookup._ID};
+        Cursor cursor = null;
+        try {
+            cursor = contactHelper.query(contactUri, projection, null, null, null);
+            if (cursor != null && cursor.moveToNext()) {
+                int personId = cursor.getColumnIndex(ContactsContract.PhoneLookup._ID);
+                return cursor.getLong(personId);
+            }
+            return -1;
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (cursor != null) {
+                cursor.close();
+                cursor = null;
+            }
+        }
+        return -1;
+    }
+}
